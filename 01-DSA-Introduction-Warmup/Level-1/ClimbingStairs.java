58aab34d289e181526568fc57b13a4dffa56b86c
panchalnehal <nehalpanchal.in@gmail.com>
2026-06-13 23:32:02 +0530

    Fibonacci and Climbing Stairns

---
+++ 01-DSA-Introduction-Warmup/Level-1/ClimbingStairs.java
@@ -0,0 +1,16 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
---
+++ 01-DSA-Introduction-Warmup/Level-1/FibonacciNumber.java
@@ -0,0 +1,27 @@
+public class FibonacciNumber {
+    // public int fib(int n) {
+    //     if(n == 0) return 0;
+    //     if(n == 1) return 1;
+    //     int a = 0; 
+    //     int b = 1;
+    //     int c = 1;
+
+    //     for(int i = 2; i<=n; i++){
+    //         c = a + b;
+    //         a = b;
+    //         b = c;
+            
+    //     }
+    //     return c;
+    // }
+
+    class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1) return n;
+
+        return fib(n-1) + fib(n-2);
+    }
+}
+// recursion
+
+}
