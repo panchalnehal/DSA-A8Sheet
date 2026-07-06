7de5a0ff0019efcceeed43fb8c2b601e788cf2ef
panchalnehal <nehalpanchal.in@gmail.com>
2026-07-06 09:25:40 +0530

    FizzBuzz

---
+++ 01-CodingFoundation/Level-1/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static List<String> fizzBuzz(int n){
+        List<String> ans = new ArrayList<>();
+        for(int i = 1; i<=n;i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                ans.add("FizzBuzz");
+            }else if(i%3 == 0){
+                ans.add("Fizz");
+            }else if(i%5 == 0){
+                ans.add("Buzz");
+            }else{
+                ans.add(String.valueOf(i));
+            }
+        }
+        return ans;
+    }
+    public static void printList(List<String> list){
+        for(int i = 0; i< list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+    public static void main(String[] args) {
+        List<String> result = fizzBuzz(15);
+        printList(result);
+    }
+}
